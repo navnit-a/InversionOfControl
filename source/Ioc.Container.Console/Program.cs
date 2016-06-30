eabb716642b1b1cc343b9034afaabdf8778e1b9e
Navnit Anuth <nanuth@qaworks.com>
2016-06-30 11:21:28 +0100

    Manual Resolver

---
+++ source/Ioc.Container.Console/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace Ioc.Container.Console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //ICreditCard creditCard = new MasterCard();
+            //ICreditCard otherCreditCard = new Visa();
+
+            Resolver resolver = new Resolver();
+            int count = 1000;
+            while (count != 0)
+            {
+                Shopper shopper = new Shopper(resolver.ResolveCreditCard());
+                shopper.Charge();
+                count--;
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
+
+public class Resolver
+{
+    public ICreditCard ResolveCreditCard()
+    {
+        var random = new Random().Next(2);
+        Console.WriteLine(random);
+        if (random == 1)
+        {
+            return new Visa();
+        }
+        return new MasterCard();
+    }
+}
+
+public class Visa : ICreditCard
+{
+    public string Charge()
+    {
+        return "Charging with the visa";
+    }
+}
+
+public class MasterCard : ICreditCard
+{
+    public string Charge()
+    {
+        return "Swiping the Master Card";
+    }
+}
+
+public class Shopper
+{
+    private readonly ICreditCard _creditCard;
+
+    public Shopper(ICreditCard creditCard)
+    {
+        _creditCard = creditCard;
+    }
+
+    public void Charge()
+    {
+        var chargeMessage = _creditCard.Charge();
+        Console.WriteLine(chargeMessage);
+    }
+}
+
+public interface ICreditCard
+{
+    string Charge();
+}
